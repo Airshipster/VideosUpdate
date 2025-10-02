e00a2e20ca8e860fcd3a8985ec1cdaaeef59fac1
Airshipster <160562833+Airshipster@users.noreply.github.com>
2025-10-02 07:29:31 +0400

    Update oauth_helper.py

--- scripts/oauth_helper.py
+++ scripts/oauth_helper.py
@@ -16,7 +16,7 @@ from googleapiclient.discovery import build
 
 # Scopes we rely on for the whole project
 SCOPES: Sequence[str] = (
-    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
     "https://www.googleapis.com/auth/spreadsheets",
 )
 
